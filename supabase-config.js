61799d0f9b97ec650d36b84003c2defa332c9cd1
xavier <xav.lair91@gmail.com>
2026-09-09 02:59:52 +0200

    add app files

---
+++ supabase-config.js
@@ -0,0 +1,7 @@
+// Don Bosco - Perfectionnement / V52
+// Cette clé est une Publishable key : elle peut être présente dans le navigateur.
+// Ne jamais mettre ici une clé sb_secret_... ou service_role.
+window.SUPABASE_CONFIG = {
+  url: 'https://zshvrarmooukeosyxbgx.supabase.co',
+  publishableKey: 'sb_publishable_6J6KrP46EpgzHUQ1U28ohA_GxiHft9V'
+};
