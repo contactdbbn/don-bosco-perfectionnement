5f354d8f4601af5e0ac60057997712361b90ff76
xavier <xav.lair91@gmail.com>
2026-09-13 17:26:52 +0200

    V94

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "don-bosco-v89";
-const SHELL = ["./", "./index.html", "./styles.css", "./app.js?v=20260911-v89", "./supabase-config.js", "./manifest.json", "./icons/icon-192.png", "./icons/icon-512.png"];
+const CACHE = "don-bosco-v94";
+const SHELL = ["./", "./index.html", "./styles.css", "./app.js?v=20260913-v94", "./supabase-config.js", "./manifest.json", "./icons/icon-192.png", "./icons/icon-512.png"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting()));
 });
